4e53a52586d28acd7ee79550113ed79274c48add
ARMEN MKRTCHYAN <armen21mkrtchyan0616@gmail.com>
2021-04-29 00:40:01 +0400

    main tasks

---
+++ class03/script.js
@@ -0,0 +1,25 @@
+// //task1
+// let admin;
+// let name = "John";
+// admin = name;
+// console.log(admin);
+// //task 2
+// let i = 10;
+// let n = i++ % 5;
+// // output i=11 n=0
+
+// //task 3
+// let a = 10;
+// let b = 3;
+// let temp = a;
+// a = b;
+// b = temp;
+// console.log(a, b);
+
+//task 4
+let a = 10;
+let b = 3;
+a = a + b;
+b = a - b;
+a = a - b;
+console.log(a, b);
