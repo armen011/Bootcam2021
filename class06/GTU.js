301c4d47e66fb801a68547813971f85134e900ff
ARMEN MKRTCHYAN <armen21mkrtchyan0616@gmail.com>
2021-05-03 13:36:49 +0400

    GTU tasks Complited

---
+++ class06/GTU.js
@@ -0,0 +1,155 @@
+////1
+//  Number("0x077") === 0x77;
+////I think this will output true
+//console.log(Number("0x077") === 0x77);
+
+// //2
+// Number("0x077") === 0x77;
+// //I think this will output true
+// console.log(Number("0x077") === 0x77);
+
+// //3
+// Number(" 12");
+// // i think this will output 12
+// console.log(Number(" 12"));
+
+// //4
+// isNaN(" ");
+// //I think this will output true XXXX
+// console.log(isNaN(" "));
+// //the output is false
+
+// //5
+// +"";
+// //i think the output will be 0
+// console.log(+"");
+
+// //6
+// 0.1 + 0.2 == 0.3;
+// // I think the output is true XXX
+// console.log(0.1 + 0.2==0.3);
+// // I was wrong the answer is fals because .1+.2= 0.30000000000000004
+
+// //7
+// 9007199254740992 + 1 == 9007199254740992;
+// //I think this is true
+// console.log(9007199254740992 + 1 == 9007199254740992);
+
+// //7
+// 9007199254740992 + 2 == 9007199254740992;
+// //I think this will output true //
+// console.log(9007199254740992 + 2 == 9007199254740992);
+// //I was wrong
+// let n = 9007199254740992 + 2;
+// console.log(n);
+
+// //8
+// 0 === +0;
+// //true
+// console.log(0 === +0);
+
+// //9
+// +0 === -0;
+// //true
+// console.log(+0 === -0);
+
+// //10
+// 1 / "2";
+// //0.5
+// console.log(1 / "2");
+
+// //11
+// 1 / 0;
+// //infinity
+// console.log(1 / 0);
+
+// //13
+// 1 / -0;
+// //-infinty
+// console.log(1 / -0);
+
+// //14
+// Infinity / 6;
+// //infinity
+// console.log(Infinity / 6);
+
+// //15
+// Infinity / -256;
+// //-infinity
+// console.log(Infinity / -256);
+
+// //16
+// Infinity / Infinity;
+// //infinty  XXX
+// console.log(Infinity / Infinity);
+// //NaN
+
+// //17
+// 0 / 0;
+// //infinity XX
+// console.log(0 / 0);
+// //NaN
+
+// //18
+// +true;
+// //1
+// console.log(+true);
+
+// //19
+// +false;
+// //0
+// console.log(+false);
+
+//20
++null;
+//undefined XXX
+console.log(+null);
+
+// //21
+// +undefined;
+// //undefined XXX
+// console.log(+undefined);
+// //NaN
+
+// //22
+// parseInt(".2");
+// //NaN
+// console.log(parseInt(".2"));
+
+// //23
+// parseInt("077a");
+// //077 XXX
+// console.log(parseInt("077a"));
+// //77
+
+// //24
+// parseInt("0x77a");
+// //0x77
+// console.log(parseInt("0x77a"));
+
+// //25
+// parseInt("I’m the best value - said Infinity.");
+// //NaN
+// console.log(parseInt("I’m the best value - said Infinity."));
+
+// //26
+// parseFloat("I’m the best value - said Infinity.");
+// //NaN
+// console.log(parseFloat("I’m the best value - said Infinity."));
+
+// //27
+// parseInt("Infinity is the best value!");
+// //infinty XXXX
+// console.log(parseInt("Infinity is the best value!"));
+// // NaN
+
+// //28
+// parseFloat("Infinity is the best value!");
+// //NaN XXX
+// console.log(parseFloat("Infinity is the best value!"));
+// //infinty
+
+// //29
+// parseFloat("12.78ff");
+// //12,78
+// console.group(parseFloat("12.78ff"));
